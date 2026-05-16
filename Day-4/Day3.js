c6b763a13d3630ba296a9bee2c03e26cdfc703e5
sahilderaiya20-droid <sahilderaiya20@gmail.com>
2026-05-16 14:01:22 +0530

    resolved conflict

--- Day-4/Day3.js
+++ Day-4/Day3.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 var x=10;
 var y=20;
@@ -31,37 +30,3 @@ document.writeln("type of l is " + typeof l +"<br>");
 document.writeln("type of m is " + typeof m +"<br>");
 document.writeln("type of n is " + typeof n +"<br>");
 
-=======
-
-var x=10;
-var y=20;
-var a=x+y;
-var b=x-y;
-var c=x*y;
-var d=x/y;
-
-document.getElementById("demo").innerHTML=("value of a is"+ a );
-document.getElementById("demo1").innerHTML=("value of b is"+ b);
-document.getElementById("demo2").innerHTML=("value of c is"+ c);
-document.getElementById("demo3").innerHTML=("value of d is"+ d);
-
-document.writeln("value of a is"+a +"<br>");
-document.writeln("value of b is"+b +"<br>");
-document.writeln("value of c is"+c +"<br>");
-document.writeln("value of d is"+d +"<br>");
-
-// typeof variable
-
-var j=20;
-var k="sahil";
-var l="true";
-var m=7.5;
-var n=[20,30,50,60,68];
-
-document.writeln("type of j is " + typeof j +"<br>");
-document.writeln("type of k is " + typeof k +"<br>");
-document.writeln("type of l is " + typeof l +"<br>");
-document.writeln("type of m is " + typeof m +"<br>");
-document.writeln("type of n is " + typeof n +"<br>");
-
->>>>>>> 68c1affa939808b36878a1de50e9a0a53e66fad3
--- Day-4/operator.js
+++ Day-4/operator.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // operators 
 
 // arithmatic operator 
@@ -17,23 +16,3 @@ document.getElementById("d4").innerHTML=("outpu of e is"+e);
 
 
 
-=======
-// operators 
-
-// arithmatic operator 
-
-var x=100+20;
-var y=20-30;
-var z=20*20;
-var w=30/20;
-var e=20*(30+40)/50;
-
-document.getElementById("d").innerHTML=("outpu of x is"+x);
-document.getElementById("d1").innerHTML=("outpu of y is"+y);
-document.getElementById("d2").innerHTML=("outpu of z is"+z);
-document.getElementById("d3").innerHTML=("outpu of w is"+w);
-document.getElementById("d4").innerHTML=("outpu of e is"+e);
-
-
-
->>>>>>> 68c1affa939808b36878a1de50e9a0a53e66fad3
