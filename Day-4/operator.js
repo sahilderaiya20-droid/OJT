80dac13e06f78ded5af25bde601c24330d96892c
sahilderaiya20-droid <sahilderaiya20@gmail.com>
2026-05-16 13:11:03 +0530

    updated ojt

---
+++ Day-4/operator.js
@@ -0,0 +1,18 @@
+// operators 
+
+// arithmatic operator 
+
+var x=100+20;
+var y=20-30;
+var z=20*20;
+var w=30/20;
+var e=20*(30+40)/50;
+
+document.getElementById("d").innerHTML=("outpu of x is"+x);
+document.getElementById("d1").innerHTML=("outpu of y is"+y);
+document.getElementById("d2").innerHTML=("outpu of z is"+z);
+document.getElementById("d3").innerHTML=("outpu of w is"+w);
+document.getElementById("d4").innerHTML=("outpu of e is"+e);
+
+
+
